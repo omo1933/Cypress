281f0181b33121bf7f69187de10b641067dc7a18
Oluwaseun Otubanjo <63556860+omo1933@users.noreply.github.com>
2021-12-17 13:58:51 -0600

    Update Home.spec.js

--- Home.spec.js
+++ Home.spec.js
@@ -1,3 +1,4 @@
+const year = 2021
 describe('Agent Home', () => {
   //visit website and login before each test
   beforeEach(() => {
@@ -18,8 +19,39 @@ describe('Agent Home', () => {
     cy.get('[ng-reflect-router-link="agent/,"] > .igx-button--outlined').should(
       'exist'
     )
-    cy.contains('Total Clicks Purchased for 2021').should('exist')
-    cy.contains('Total Clicks Used during 2021').should('exist')
-    cy.contains('Total Clicks Remaining for 2021').should('exist')
+    //Total Clicks
+    cy.contains(`Total Clicks Purchased for ${year}`).should('exist')
+    cy.wait(1000)
+    cy.get('.usage-numbers').then(($usageNumbers) => {
+      const clicksPurchased = $usageNumbers.first().text()
+      const clicksUsed = $usageNumbers.eq(1).text()
+      const clicksRemaining = $usageNumbers.eq(2).text()
+      const expectedPurchased = parseInt(clicksUsed) + parseInt(clicksRemaining)
+      const expectedClicksUsed =
+        parseInt(clicksPurchased) - parseInt(clicksRemaining)
+      const expectedClicksRemaining =
+        parseInt(clicksPurchased) - parseInt(clicksUsed)
+      cy.log(clicksRemaining)
+      cy.log(clicksUsed)
+      cy.log(clicksPurchased)
+      expect(clicksPurchased).to.eq(expectedPurchased.toString())
+      expect(clicksUsed).to.eq(expectedClicksUsed.toString())
+      expect(clicksRemaining).to.eq(expectedClicksRemaining.toString())
+    })
+    cy.contains(`Total Clicks Used during ${year}`).should('exist')
+    cy.contains(`Total Clicks Remaining for ${year}`).should('exist')
+    //Quarterly Usage Summary Graphy
+    cy.get('h3').invoke('text').should('eq', 'Quarterly Usage Summary')
+    cy.get('.ig-chart-legend-item-text')
+      .eq(0)
+      .invoke('text')
+      .should('eq', '2020')
+    cy.get('.ig-chart-legend-item-text')
+      .eq(1)
+      .invoke('text')
+      .should('eq', '2021')
+    cy.get(
+      '[style="position: absolute; top: 0px; left: 0px; touch-action: none; user-select: none; width: 600px; height: 425px; outline: 0px;"]'
+    ).trigger('mouseover')
   })
 })
